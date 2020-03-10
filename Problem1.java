2e74b87e51aa18d08c5bc102a40138c967612e5e
KetanKodmelwar <ketankodmelwar@gmail.com>
2020-03-09 17:59:18 -0700

    Problem 1 attempted. Not solved

---
+++ Problem1.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> list=new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        Arrays.sort(candidates);
+        addVal(candidates,target,0,new ArrayList<Integer>());
+        return list;
+
+        
+    }
+    
+    private void addVal(int[] candidates, int target, int start,List<Integer> rec){
+        
+        for(int i=start;i<candidates.length;i++){
+            List<Integer> temp=new ArrayList<>(rec);
+            if(candidates[i]==target){
+                temp.add(candidates[i]);
+                list.add(temp);
+                break;
+            }
+            else if(candidates[i]<target){
+                temp.add(candidates[i]);
+                addVal(candidates,target-candidates[i],i,new ArrayList<>(rec));
+            }else{
+                break;
+            }
+        }
+        
+    }
+}
